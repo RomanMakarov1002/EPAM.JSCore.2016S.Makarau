6f47d83c71f79d403630844cd02bc02f5d2a4761
RomanMakarov1002 <roman.makarov1002@gmail.com>
2016-08-21 13:52:24 +0300

    tesk task

--- task3/script.js
+++ task3/script.js
@@ -7,64 +7,68 @@ $(document).ready(function(){
 	$ResetButton = $('#Reset');
 	
 	if ($ContentContainer.children().length === 0){
-		disableButton($SetColorButton);										
-		disableButton($ResetButton);
+		disableButton($SetColorButton, setColor);										
+		disableButton($ResetButton, reset);
 	};
 
-	$GenerateButton.on('click', function(){
-		if ($ContentContainer.children().length > 0){
-			$ContentContainer.empty();
-		}
-		for (var i = 0; i < blocksCount; i++){
-			$ContentContainer.append('<div class="block-content"><p>' + random(1, 100) + '</p></div');			
-		}
-		disableButton($GenerateButton);
-		enableButton($SetColorButton);
-		enableButton($ResetButton);
-	});
+	$GenerateButton.click(generate); 
+	$SetColorButton.click(setColor);
+	$ResetButton.click(reset);
 
-	$SetColorButton.on('click', function(){
-		disableButton($SetColorButton);
-
-		$blocks = $('.block-content');
-		$blocks.each(function(){
-			var $this = $(this);
-			if ($this.text() > 75){
-				$this.css('background-color', '#f44336');
-				$this.css('box-shadow', '0px 0px 0px 2px white');
-			}
-			else if ($this.text() > 50){
-				$this.css('background-color', '#ff9800');
-				$this.css('box-shadow', '0px 0px 0px 2px white');
-			}
-			else if ($this.text() > 25){
-				$this.css('background-color', '#4caf50');
-				$this.css('box-shadow', '0px 0px 0px 2px white');
-			}
-		});
-	});
-
-	$ResetButton.on('click', function(){
-		$ContentContainer.empty();
-		disableButton($SetColorButton);
-		disableButton($ResetButton);
-		enableButton($GenerateButton);
-	});
 });
 
-function disableButton(button){
-	button.css('opacity', 0.25);
-	button.css('cursor', 'not-allowed');
-	button.prop('disabled', true);
+
+//custom functions for disabling/enabling div buttons
+function disableButton(button, f){
+	button.removeClass('enabled-div-button');
+	button.addClass('disabled-div-button');
+	
+	button.unbind('click', f);
 }
 
-function enableButton(button){
-	button.css('opacity', 1);
-	button.css('cursor', 'pointer');
-	button.prop('disabled', false);
+function enableButton(button, f){
+	button.removeClass('disabled-div-button');
+	button.addClass('enabled-div-button');
+	button.click(f);
 }
 
 function random(min, max) {
 	return Math.floor((Math.random() * max) + min);
 }
 
+function generate(){
+	if ($ContentContainer.children().length > 0){
+		$ContentContainer.empty();
+	}
+	for (var i = 0; i < blocksCount; i++){
+		$ContentContainer.append('<div class="block-content"><p>' + random(1, 100) + '</p></div>');			
+	}
+	disableButton($GenerateButton, generate);
+	enableButton($SetColorButton, setColor);
+	enableButton($ResetButton, reset);
+}
+
+function reset(){
+	$ContentContainer.empty();
+	disableButton($SetColorButton, setColor);
+	disableButton($ResetButton, reset);
+	enableButton($GenerateButton, generate);
+}
+
+function setColor(){
+	disableButton($SetColorButton, setColor);
+
+	$blocks = $('.block-content');
+	$blocks.each(function(){
+		var $this = $(this);
+		if ($this.text() > 75){
+			$this.addClass('red-block');
+		}
+		else if ($this.text() > 50){
+			$this.addClass('orange-block');
+		}
+		else if ($this.text() > 25){
+			$this.addClass('green-block');
+		}
+	});
+}
